871b25ca0eded36cd9346e0a3cbcc10c1f98c08c
D.Patel <d.patel@interproductec.com>
2021-01-02 19:45:24 +0100

    add embed.go

--- chap_4/embed.go
+++ chap_4/embed.go
@@ -15,23 +15,23 @@ type Wheel struct {
 }
 
 func main(){
-	// declaration examples for anonymous fields
-	// two anonymous fields of same type is not allowed
-	w := Wheel{Circle{Point{8, 8}, 5}, 20}
-	w = Wheel{
-	Circle: Circle{
-	Point: Point{X: 8, Y: 8},
-	Radius: 5,
-	},
-	Spokes: 20, // NOTE: trailing comma necessary here (and at Radius)
-	}
-	// the adverb # prints the field names
+    // declaration examples for anonymous fields
+    // two anonymous fields of same type is not allowed
+    w := Wheel{Circle{Point{8, 8}, 5}, 20}
+    w = Wheel{
+    Circle: Circle{
+    Point: Point{X: 8, Y: 8},
+    Radius: 5,
+    },
+    Spokes: 20, // NOTE: trailing comma necessary here (and at Radius)
+    }
+    // the adverb # prints the field names
 
-	fmt.Printf("%#v\n", w)
-	// Output:
-	// Wheel{Circle:Circle{Point:Point{X:8, Y:8}, Radius:5}, Spokes:20}
-	w.X = 42
-	fmt.Printf("%#v\n", w)
-	// Output:
-	// Wheel{Circle:Circle{Point:Point{X:42, Y:8}, Radius:5}, Spokes:20}	
+    fmt.Printf("%#v\n", w)
+    // Output:
+    // Wheel{Circle:Circle{Point:Point{X:8, Y:8}, Radius:5}, Spokes:20}
+    w.X = 42
+    fmt.Printf("%#v\n", w)
+    // Output:
+    // Wheel{Circle:Circle{Point:Point{X:42, Y:8}, Radius:5}, Spokes:20}    
 }
