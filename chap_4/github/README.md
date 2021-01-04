d1071a1f4293f729d44ec2c8974bda4b1b23a39c
D.Patel <d.patel@interproductec.com>
2021-01-04 18:58:00 +0100

    add package github demo code

---
+++ chap_4/github/README.md
@@ -0,0 +1,17 @@
+# Introduction
+
+This is the code from "The Go Programming Language" Book, Chapter-4, Section 4.5. 
+It demonstrates the use of JSON API along with HTTP API. 
+
+# Instructions
+To build the package run 
+```
+go mod init github
+```
+
+This will create the .mod file. To run the package,
+```
+cd main
+go run issues.go is:open json decoder
+```
+ 
---
+++ chap_4/github/github.go
@@ -0,0 +1,29 @@
+// demonstration of using json interface with http requests
+// See https://developer.github.com/v3/search/#search-issues.
+
+package github
+
+import "time"
+
+const IssuesURL = "https://api.github.com/search/issues"
+
+type IssuesSearchResult struct {
+	TotalCount  int	`json:"total_count`  // field tag to convert field TotalCount to total_count for json
+	Items		[]*Issue	
+}
+
+// struct names have to be capitalized if they are to be imported along with the package
+type Issue struct {
+	Number		int
+	HTMLURL		string `json:"html_url"`
+	Title		string
+	State		string
+	User		*User
+	CreatedAt	time.Time   `json:"created_at"`
+	Body		string	// in Markdown format
+}
+
+type User struct {
+	Login	string
+	HTMLURL	string `json:"html_url"`
+}
